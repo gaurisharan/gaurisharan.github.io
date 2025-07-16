d72d8d1d5fc32c76dfdc6b8db3e2d1b4efefacf6
gaurisharan <gaurisharan123@gmail.com>
2025-07-16 16:19:00 +0530

    Added more weblinks.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -15,14 +15,14 @@ export default function Experience() {
       description: "Worked on RAG, Vision Language Models, and finetuning at PARIMAL Labs."
     },
     {
-      company: "Outlier AI",
+      company: "Outlier",
       role: "Freelance GenAI Developer",
       duration: "Oct 2024 - Dec 2024",
       description: "Trained LLMs with RLHF, focused on Python/SQL code generation (Soul AI)."
     },
     {
-      company: "Sapphire Applications",
-      role: "Cloud & Data Intern",
+      company: "Whiteswan Security",
+      role: "Cloud & Data Intern (Remote)",
       duration: "Jul 2024 - Nov 2024",
       description: "Automated AWS infra using Boto3, Docker, and CI/CD pipelines."
     },
@@ -31,6 +31,12 @@ export default function Experience() {
       role: "Data Analyst Intern",
       duration: "Nov 2023 - May 2024",
       description: "Built analytics dashboards and insights for FutureSkills Prime."
+    }
+    {
+      company: "Sapphire Applications",
+      role: "Full Stack Developer (Fulltime)"
+      duration: "Oct 2022 - Oct 2023",
+      description: "Worked on Fulldtack end-to-end projects with Django, Python etc."
     }
   ]
   return (
